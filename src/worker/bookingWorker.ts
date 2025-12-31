4183c410bc579eeec9d64e5f5160d793e4ea87b1
Abhishek456321 <abhishek.khadka.499@gmail.com>
2025-12-31 16:32:46 +0545

    bullMQ

--- src/worker/bookingWorker.ts
+++ src/worker/bookingWorker.ts
@@ -2,12 +2,9 @@ import { Worker, Job } from "bullmq";
 import redis from "../config/redis";
 
 export function startBookingWorker() {
-  // first argument same hunuparxa queue name sanga so that it will perform the job schedule in the queue  new Queue()
   const bookingWorker = new Worker(
     "notify",
     async (job: Job) => {
-      // job.data is 2 argu in queue.add("",data)
-      // job.name is 1 argu in queue.add(name,data,)
       const { type, data } = job.data;
       console.log(
         ".................//////---> whatever job to performed mail, sms,etc ."
